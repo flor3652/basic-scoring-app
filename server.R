6b9ba86bfc8daeaa0c0da67dca9c020f42ad9c8f
Michael Floren <flor3652@bears.unco.edu>
2015-08-24 15:56:34 -0600

    Serious names

--- server.R
+++ server.R
@@ -3,12 +3,12 @@ library(shiny)
 shinyServer(function(input, output){
   
   output$name1out <- renderText({
-    input$gogogo
+    input$update
     isolate(input$name1)
   })
   
   output$name2out <- renderText({
-    input$gogogo
+    input$update
     isolate(input$name2)
   })
   
--- ui.R
+++ ui.R
@@ -8,8 +8,8 @@ shinyUI(fluidPage(
     sidebarPanel(
       textInput("name1", "What is the name of the first player?", ""),
       textInput("name2", "What is the name of the second player?", ""),
-      sliderInput("winningScore", "How many points are needed to win?", 60, 120, 60, 120),
-      actionButton("gogogo", "annnnnd Action!")
+      sliderInput("winningScore", "How many points are needed to win?", 60, 120, 60, 10),
+      actionButton("update", "Update")
     ),
     
     mainPanel(
